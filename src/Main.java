37bf2c806191340c0a686204abe12cb2cf202339
Buddya Tsebekov <tsebekov94@gmail.com>
2022-01-03 18:32:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        //демонстрация
+        System.out.println(bins.sum("11101000", "11101000"));
+        System.out.println(bins.mult("11101000", "00000010"));
+    }
+}
